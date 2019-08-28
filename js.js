79409448311c91556dff3b837f3f10d7709fa4e5
detindiskeocean <5884450@rts-365.dk>
2019-08-28 14:00:58 +0200

    contact HTML + CSS

---
+++ js.js
@@ -0,0 +1,8 @@
+document.addEventListener ("DOMContentLoaded", () => {
+
+	// document.querySelector("#frontpage").addEventListener("click", () => {
+	// 	document.querySelector("#main-section").style.display = "block"
+	// 	document.querySelector("#contact").style.display = "none"
+	// }) 
+
+})
